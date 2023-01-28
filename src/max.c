596bd618b2ea947b379f745746a5525a7baca933
Zheng Zheng <zzheng0620@gmail.com>
2023-01-27 21:35:42 -0500

    Add files via upload

---
+++ src/max.c
@@ -0,0 +1,19 @@
+/*******************************************************************************
+*
+* Purpose: Prints the maximum number of any number of int command-line 
+* arguments that are provided.  So if this program is run with:
+*   ./max 5 10 20 25 15
+* then we can expect it to print 25.
+*
+*******************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include "library.h"
+
+int main(int argc, char *argv[])
+{
+
+
+
+  return 0;
+}
---
+++ src/square.c
@@ -0,0 +1,16 @@
+/*******************************************************************************
+*
+* Purpose: Square area calculator.  Outputs caculated square areas from 
+* side length provided via standard input... outputs them with a unit 
+* provided as 2nd argv values.  i.e. if we run with ""./square inches" expect 
+* output of the format: 50 inches
+*
+*******************************************************************************/
+#include <stdio.h>
+#include "library.h"
+
+int main(int argc, char *argv[])
+{
+
+  return 0;
+}
