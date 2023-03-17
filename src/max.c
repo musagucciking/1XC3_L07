1e0a805497ffa3f17c563f60c6c44355a5243cc1
sheikm29 <sheikm29@pascal.cas.mcmaster.ca>
2023-03-17 17:01:22 -0400

    Final File

--- src/max.c
+++ src/max.c
@@ -1,19 +1,33 @@
-/*******************************************************************************
-*
-* Purpose: Prints the maximum number of any number of int command-line 
-* arguments that are provided.  So if this program is run with:
-*   ./max 5 10 20 25 15
-* then we can expect it to print 25.
-*
-*******************************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
 #include "library.h"
 
 int main(int argc, char *argv[])
 {
+ 
 
+  // Check if the correct number of arguments was provided
+  if (argc < 2) 
+  {
+    printf("Error: Incorrect number of arguments provided.\n");
+    return 0;
+  }
 
+  // Convert arguments to array of integers
+  argc -= 1;
+  int arg_arr[argc];
+
+  for (int i = 0; i < argc; i++)
+  {
+    if ((arg_arr[i] = atoi(argv[i+1])) == EOF) {
+      printf("Error: Invalid list of numbers given\n");
+      return 0;
+    }
+  }
+
+  // Calculate and print the maximum value in the array
+  printf("%d\n", max(arg_arr, argc));
 
   return 0;
 }
+
